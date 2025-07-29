cd8acbb48f763521ebac9918e9d19b8ef25188e9
St3ckd0ze <thomas.wiethaup@web.de>
2025-07-29 22:59:06 +0200

    mc-manager first init

--- src/ApplicationLoader.ts
+++
@@ -1,6 +0,0 @@
-import { ApplicationManager } from "./ApplicationManager.js";
-
-window.addEventListener("DOMContentLoaded", () => {
-    const appManager = new ApplicationManager();
-    appManager.loadLandingPage();
-});
